76e1ba4436d13c94bfeee6f1c99f776396ab5b0a
lazicah <lazicah@gmail.com>
2026-05-06 00:10:03 +0100

    style: Format validateStatus function for better readability

--- lib/core/network/http_service.dart
+++ lib/core/network/http_service.dart
@@ -18,7 +18,8 @@ class HttpService {
           connectTimeout: const Duration(seconds: 60),
           receiveTimeout: const Duration(seconds: 60),
           sendTimeout: const Duration(seconds: 60),
-          validateStatus: (status) => status != null && status >= 200 && status < 300,
+          validateStatus: (status) =>
+              status != null && status >= 200 && status < 300,
           headers: {
             'Accept':
                 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
